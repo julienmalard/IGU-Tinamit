a3dd078a6adf78ecfb14b8d0b5a0aaf9432c428c
Julien Malard <julien.malard@mail.mcgill.ca>
2018-06-18 14:08:24 -0400

    Jub'a' samaj chïk.

--- ruxe'el/nik'ajal.js
+++ ruxe'el/nik'ajal.js
@@ -8,7 +8,14 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    width: 1200,
+    height: 900,
+    minWidth: 800,
+    minHeight: 600,
+    icon: path.join(__dirname, "chjl/wach'ib'äl/tinamìt_ko'ol.png")
+  })
+  win.setMenu(null)
 
   // and load the index.html of the app.
   win.loadURL(url.format({
